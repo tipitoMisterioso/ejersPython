52db4786be9caf97ab1f4b741905581b44244a33
tipitoMisterioso <dariogorostegui@gmail.com>
2023-03-12 20:40:27 -0300

    :tada: Begin the project

---
+++ 1.py
@@ -0,0 +1,5 @@
+#1 - Ver la version de python instalada
+
+import sys
+
+print("La version de python instalada es la", sys.version)
---
+++ 2.py
@@ -0,0 +1,7 @@
+#Utilzar placeholder. sep y end en el print
+
+print("Esto cambio el caracteres final del print", end='@')
+
+#le aplico un salto de linea para que no quede todo juntols
+
+print("Esto cambiar el caracteres de separacion de las palabras", sep='.')
