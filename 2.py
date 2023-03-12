92cde379e0dc76b7cef67690c70afe6e869fe1d8
tipitoMisterioso <dariogorostegui@gmail.com>
2023-03-12 20:57:08 -0300

    :sparkles: Add print with sep, end and placeholder

--- 2.py
+++ 2.py
@@ -2,6 +2,10 @@
 
 print("Esto cambio el caracteres final del print", end='@')
 
-#le aplico un salto de linea para que no quede todo juntols
+#le aplico un salto de linea para que no quede todo junto
+print()
 
 print("Esto cambiar el caracteres de separacion de las palabras", sep='.')
+
+datos = {"nombre": "Dario", "ciudad":"Mar del Plata"}
+print("Utilzamos placeholder. Mi nombre es {} y soy de la ciudad de {}" .format(datos["nombre"], datos["ciudad"]))
